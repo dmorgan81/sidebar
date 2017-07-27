a295560c8c959508a0bf0d51453da35a5aa0bf43
David Morgan <dmorgan81@gmail.com>
2017-07-27 15:45:37 -0400

    Don't allow the same widget to occupy two spaces

--- src/pkjs/custom-clay.js
+++ src/pkjs/custom-clay.js
@@ -4,5 +4,18 @@ module.exports = function(minified) {
     var $ = minified.$;
     var HTML = minified.HTML;
 
-    Clay.on(Clay.EVENTS.AFTER_BUILD, function() {});
+    Clay.on(Clay.EVENTS.AFTER_BUILD, function() {
+
+        var widgets = Clay.getItemsByGroup('widget');
+        widgets.forEach(function(widget) {
+            widget.on('change', function() {
+                widgets
+                    .filter(function(w) { return w !== this; }, this)
+                    .filter(function(w) { return w.get() !== '0'; })
+                    .filter(function(w) { return w.get() === this.get(); }, this)
+                    .forEach(function(w) { w.set('0'); });
+            });
+        });
+
+    });
 }
